a667c246bffdf24aaba6a983f8ba1b19408a1ae0
Dohner, James Thomas <jtdohner@iu.edu>
2026-04-28 14:56:56 -0400

    interaction
    Added small user interaction when over over day

--- app/animate.js
+++ app/animate.js
@@ -1,15 +1,16 @@
 // import { gsap } from "gsap";
 
 export function animate() { 
-    const tl = gsap.timeline();
 
-    tl.fromTo(
+    const t2 = gsap.timeline();
+
+    t2.fromTo(
         "#curentContaner",
         { y: 100, opacity: 0 },
         {
             y: 0,
             opacity: 1,
-            duration: 1.5,
+            duration: 1,
             ease: "power3.out"
         }
     )
@@ -21,7 +22,21 @@ export function animate() {
                 opacity: 1,
                 duration: 1.5,
                 ease: "power3.out",
-                stagger: 1
+                stagger: 1,
+                onComplete: () => {
+                    document.querySelectorAll(".dayBar").forEach((bar) => {
+                        bar.addEventListener("mouseenter", () => {
+                            gsap.killTweensOf(bar);
+                            gsap.to(bar, {
+                                x: 20,
+                                duration: 0.06,
+                                repeat: 5,
+                                yoyo: true,
+                                ease: "power1.inOut"
+                            });
+                        });
+                    });
+                }
             }
         );
 }
